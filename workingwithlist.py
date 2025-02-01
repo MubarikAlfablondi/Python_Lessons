917e8cf3f37722fd8289072536f27471c6a3e24e
Vant03 <mubarikalfablondi@protonmail.com>
2025-01-31 17:03:22 -0800

    01/31/25 Update Chapter4

---
+++ workingwithlist.py
@@ -0,0 +1,14 @@
+workers = ['bob', 'harlod', 'synthia', 'mike']
+for work in workers:
+    print(work)
+# so for every one in work in the list of workers, print out the list of names from work
+
+workers = ['bob', 'harlod', 'synthia', 'mike']
+for work in workers:
+    print(f"that was great {work.title()}. keep it up")
+# this was an example of an f-string being a great way towards showing the for loop can be used within the list and f-string.abs
+
+#Note: Remember that indentation is important to keep a variable into its contained lists. Especially with two print functions. There can be a case of logicall error if not checking correctly 
+
+
+
